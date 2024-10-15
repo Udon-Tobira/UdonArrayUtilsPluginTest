8f1f893b8abb564bba0dd50cff4b644b7c6edf0e
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2024-10-15 10:34:02 +0900

    fixed for UE5.* versions

--- Source/UdonArrayUtilsPluginTest/UdonArrayUtilsPluginTest.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "UdonArrayUtilsPluginTest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UdonArrayUtilsPluginTest, "UdonArrayUtilsPluginTest" );
